c09a72c365dfb69ef53ed1be197fc4dc89ddc10c
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-09-07 18:17:12 -0500

    looking for a none es6 way, this push is wrong, will come back to

---
+++ js/isTrue.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let isTrueArr = [1,2,3,4,5,6,7,8,'a','v','s','w','d','g','h','i','j','k',true];
+// warm up push, for js
+
+for (let i = 0; i < isTrueArr.length; i++) {
+    if (isTrueArr[i] === true) {
+        console.log(isTrueArr[i]);
+    }
+}
+
+console.log();
