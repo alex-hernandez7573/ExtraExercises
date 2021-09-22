798d1e7fd34950c49a0c257978025300bed09458
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-09-21 22:04:29 -0500

    warm up to push, hacker rank work

---
+++ Test/src/scr5.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class scr5 {
+    public static void main(String[] args) {
+        System.out.print("loops");
+        // simple while loop that counts 0-4
+        int a = 0;
+        while (a < 5) {
+            System.out.print(a);
+            a++;
+        }
+        String carName = "volvo";
+        // do while that counts
+        int b = 0;
+        do {
+            System.out.println(b);
+            b++;
+        }
+        while (b < 5);
+        //for loop that finds the multiples of inputted number
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+
+            System.out.printf("%d x %d = %d%n", N, i, N*i);
+        }
+
+
+
+
+
+
+
+
+    }
+}
