4469ca5ed76ff56f9aef5d055fc7440592388587
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-11-06 14:26:42 -0500

    rough sketch of simple calculator

---
+++ Test/src/SimpleCalculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+
+    // application.
+    // we want to add
+    // we want to subtract
+    // we want to divide
+
+    public static void main(String[] args) {
+
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter your first number");
+            int userNumA = scanner.nextInt();
+            System.out.println("Please enter your second number");
+            int userNumB = scanner.nextInt();
+
+            addTwoNums(userNumA ,userNumB );
+            subtractTwoNums(userNumA ,userNumB );
+            dividingTwoNums(userNumA ,userNumB );
+
+
+        }
+
+    }
+
+
+    public static void addTwoNums (int userNumA, int userNumB ) {
+        System.out.println("adding ...");
+        System.out.println(userNumA + userNumB);
+    }
+
+    public static void subtractTwoNums (int userNumA, int userNumB) {
+        System.out.println("subtracting ...");
+        System.out.println(userNumA - userNumB);
+    }
+    public static void dividingTwoNums (int userNumA, int userNumB) {
+        System.out.println("dividing ...");
+        System.out.println(userNumA / userNumB);
+    }
+
+}
